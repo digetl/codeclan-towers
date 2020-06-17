53de1bbd44f6028bdb13c3b08769e6f9a7a4f896
digetl <dave@digetl.co.uk>
2020-06-17 11:35:50 +0100

    Create tests for Bedroom

---
+++ src/main/java/Bedroom.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Bedroom {
+    private String roomName;
+    private int capacity;
+    private String type;
+    private ArrayList<Guest> guests;
+
+    public Bedroom(String roomName, int capacity, String type) {
+        this.roomName = roomName;
+        this.capacity = capacity;
+        this.type = type;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public String getRoomName() {
+        return this.roomName;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public ArrayList<Guest> countGuests() {
+        return guests;
+    }
+}
---
+++ src/test/java/BedroomTest.java
@@ -0,0 +1,33 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BedroomTest {
+
+    private Bedroom bedroom;
+
+    @Before
+    public void before() {bedroom = new Bedroom("27", 2, "double")};
+
+    @Test
+    public void hasRoomName() {
+        assertEquals("27", bedroom.getRoomName());
+    }
+
+    @Test
+    public void hasCapacity() {
+        assertEquals(2, bedroom.getCapacity());
+    }
+
+    @Test
+    public void hasType() {
+        assertEquals("double", bedroom.getType());
+    }
+
+    @Test
+    public void bedroomStartsEmpty() {
+        assertEquals(0, bedroom.countGuests());
+    }
+
+}
